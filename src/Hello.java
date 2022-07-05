0b754140af1ff2d6101d3f9fb40bdd7e62ea490a
Zachary Liu <yicun.liu@foxmail.com>
2022-07-05 13:53:10 +0800

    master commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("second try");
         System.out.println("ok ? ");
+
+        System.out.println("master test");
     }
 }
