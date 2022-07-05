a8e03f4e399c3c6bb1f077098874f7c0ce05f8fb
Zachary Liu <yicun.liu@foxmail.com>
2022-07-05 13:39:20 +0800

    forth commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("second try");
         System.out.println("ok ? ");
+        System.out.println("hot-fix merge OP REQUEST!");
     }
 }
